fb6e5491da41006129819cdd52657226938d21ca
FloweyAndinov <flowey.andinov@gmail.com>
2023-10-22 22:19:00 +0300

    Pop-up ready

--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -1,13 +1,28 @@
 import React from 'react'
+import checkWin from './helpers';
+const Popup = ({correctLetters , wrongLetters , selectedWord , setPlayable}) => {
+  let finalMessage = '';
+  let finalMessageRevealWord = '';
+  let playable = true;
 
-const Popup = () => {
+  if (checkWin(correctLetters,wrongLetters,selectedWord) === 'win') {
+    console.log("win");
+    finalMessage = 'You win!';
+    playable=false;
+  }
+  else if (checkWin(correctLetters,wrongLetters,selectedWord) === 'lose') {
+    console.log("lose");
+    finalMessage = 'You lost!';
+    finalMessageRevealWord = `The word was '${selectedWord}'` ;
+    playable=false;
+  }
   return (
     // <!-- Container for final message -->
-    <div class="popup-container" id="popup-container">
+    <div className="popup-container" style={finalMessage !=='' ? {display:'flex'} : {}} >
       <div class="popup">
-        <h2 id="final-message"></h2>
-        <h3 id="final-message-reveal-word"></h3>
-        <button id="play-button">Play Again</button>
+        <h2>{finalMessage}</h2>
+        <h3 >{finalMessageRevealWord}</h3>
+        <button>Play Again</button>
       </div>
     </div>
   )
--- src/components/helpers.jsx
+++ src/components/helpers.jsx
@@ -4,3 +4,21 @@ export function showNotification(setter) {
         setter(false);
     } , 1000)
 }
+const checkWin = function(correct,wrong,word) {
+    let status='win';
+    console.log(wrong.length);
+    //check for win
+    word.split('').forEach(letter => {
+        if(!correct.includes(letter)) {
+            status='';
+        }
+    })
+
+    //check for loss
+    if (wrong.length >= 6) {
+        status='lose';
+    } 
+
+    return status;
+}
+export default checkWin;
